5b564583602e1dcfbb3ab2c5a8578e0fdde18b32
preetikrp <preetikrp@gmail.com>
2017-07-17 00:23:09 -0500

    Squared even number exercise.

--- Squared Randoms/Program.cs
+++ Squared Randoms/Program.cs
@@ -10,6 +10,54 @@ namespace Squared_Randoms
     {
         static void Main(string[] args)
         {
+
+            /*
+              Using the Random class, generate a list of 20 random numbers that are in the range of 1-50.
+
+                Random random = new Random();
+                // Create a list
+                // Populate the list
+                With the resulting List, populate a new List that contains each number squared. For example, if the original list is 2, 5, 3, 15, the final list will be 4, 25, 9, 225.
+
+                Then remove any number that is odd from the list of squared numbers.
+             */
+            Random random = new Random();
+            List<int> numSquare = new List<int>();
+            for (int i = 0; i <= 20; i++)
+            {
+                numSquare.Add (random.Next(1, 50));
+
+            }
+            List<int> squaredNew = new List<int>();
+
+            foreach(int num in numSquare)
+            {
+                squaredNew.Add(num * num);
+                
+            }
+            Console.WriteLine("Squared even numbers");
+            Console.WriteLine("=====================");
+
+            foreach (int num2 in squaredNew.ToList()) 
+            {
+                
+
+                if( num2 % 2 != 0)
+                {
+                    squaredNew.Remove(num2);
+                }
+                else
+                {
+                    
+                    Console.WriteLine(num2);
+
+                }
+
+
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
